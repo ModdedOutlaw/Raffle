ee044f2ce5c3139f94dcbaa5a0c27c306cc915f6
ModdedOutlaw <moddedoutlaw33@gmail.com>
2022-02-26 16:23:31 -0500

    x

--- transferList.js
+++ transferList.js
@@ -8,56 +8,19 @@
   fetch(urlRaffleEntries).then(function (res) {
     return res.json()
   }).then(function (data) {
-
-    let list = data.data;
-    console.log(list);
-
     
-
-   /* var sortedlist = [];
-    var sortedarray = [];
-    var newlist = new Array();
-
-    for (let i = 0; i < list.length; i++) {
-
-      sortedlist.push([list[i].sender_name, Object.keys(list[i].assets).length]);
-    }
-    sortedarray = sortedlist.slice().sort();
-
-   
-    let i = 0;
-    let k = 0;
-    let length = sortedarray.length;
-
-    console.log(sortedarray);
-
-    while (i < length) {
-      if (i === 0) {
-        newlist.push(sortedarray[i]);
-        k++;
-        i++;
-      } else {
-        if (sortedarray[i][0] === newlist[k - 1][0]) {
-          newlist[k - 1][1] += sortedarray[i][1];
-          i++;
-        } else {
-          newlist.push(sortedarray[i]);
-          i++;
-          k++;
-        }
-      }
-    }
-
-  let tt = 0;*/
+    let list = data.data;
+    let ms = list[0].created_at_time*1000;
+    var time_transfered = new Date(ms);
+    console.log(list);
+    console.log(time_transfered);
+  
     
   for(let i = 0; i < list.length; i++){
-       outputRaffleEntries.innerHTML += '<br><b>' + (i+1) + '. </b>' + list[i].sender_name + '<br>';
+       let ms = list[i].created_at_time*1000;
+       var time_transfered = new Date(ms);
+       outputRaffleEntries.innerHTML += '<tr><td><b>' + (i+1) + '.</td> </b><td>' + list[i].sender_name + '</td> <td class = "time"> ' + time_transfered +'</tr>';
     }
-    console.log(newlist);
-
-    //outputTransfers.innerHTML += '<br><b>'+tt+'</b><br>';
-
-    
 
   }).catch(function (error) {
     console.log(error);
