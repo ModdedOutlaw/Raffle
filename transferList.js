96a95a5759a869c2852d0810731c8e730362e3d7
ModdedOutlaw <moddedoutlaw33@gmail.com>
2022-03-06 15:01:59 -0500

    x

--- transferList.js
+++ transferList.js
@@ -111,7 +111,7 @@ async function fetchCommonMinerPrice() {
 
       let list = data.data;
       
-      console.log("Median Price Common Miner #" + commonMinerArray[i] + " = " + list[0].median / 100000000);
+      //console.log("Median Price Common Miner #" + commonMinerArray[i] + " = " + list[0].median / 100000000);
       i++;
 
     }).catch(function (error) {
@@ -152,7 +152,7 @@ async function fetchCommonMinerSales() {
 
   i = 0;
 
-  console.log(minerUrlArray);
+  //console.log(minerUrlArray);
 
   const imgBegin = "AboutPageAssets/images/";
   const saleLink = "https://wax.atomichub.io/market/sale/";
@@ -166,9 +166,9 @@ async function fetchCommonMinerSales() {
 
       let list = data.data;
 
-      console.log(list);
+      //console.log(list);
 
-      console.log(list[0].sale_id);
+      //console.log(list[0].sale_id);
 
       let imgUrl = imgBegin.concat(list[0].assets[0].template.immutable_data.img);
 
@@ -186,6 +186,97 @@ async function fetchCommonMinerSales() {
     })
   });
   outputCommonMinerSales.innerHTML += "</table>";
+
+
+}
+
+
+async function fetchLandKeySales() {
+
+  const urlLandKeySalesStart = "https://wax.api.atomicassets.io/atomicmarket/v1/sales?state=1&collection_name=upliftworld&schema_name=keys&template_id="
+  const urlLandKeySalesBlankEnd = "&page=1&limit=100&order=asc&sort=price";
+  
+  const outputLandKeySales = document.querySelector('.outputLandKeySales');
+
+  const arrayLandKeys = [172596,110840,121669,172599]
+
+  const landKeyUrlArray = [];
+
+  outputLandKeySales.innerHTML = "";
+
+  let i = 0;
+
+  arrayLandKeys.forEach(element => {
+    landKeyUrlArray[i] = urlLandKeySalesStart.concat(element);
+    landKeyUrlArray[i] = landKeyUrlArray[i].concat(urlLandKeySalesBlankEnd);
+    i++;
+  });
+
+  i = 0;
+
+  console.log(landKeyUrlArray);
+
+  const imgBegin = "AboutPageAssets/images/";
+  const saleLink = "https://wax.atomichub.io/market/sale/";
+
+  outputLandKeySales.innerHTML += "<table>";
+  landKeyUrlArray.forEach(element => {
+
+    fetch(element).then(function (res) {
+      return res.json()
+    }).then(function (data) {
+
+      let list = data.data;
+      let Upluft = 0;
+      let Londom = 0;
+      let chikinKaah = 0;
+
+      list.forEach(element => {
+        //console.log(element.assets[0].immutable_data.world);
+
+        if(element.assets[0].immutable_data.world == "Londom" && Londom == 0){
+          let saleUrl = saleLink.concat(element.sale_id)
+          console.log(element.sale_id);
+          console.log(element.assets[0].immutable_data.world);
+          console.log(saleUrl);
+          Londom = 1;
+        }
+        if(element.assets[0].immutable_data.world == "Upluft" && Upluft == 0){
+          let saleUrl = saleLink.concat(element.sale_id)
+          console.log(element.sale_id);
+          console.log(element.assets[0].immutable_data.world);
+          console.log(saleUrl);
+          Upluft = 1;
+        }
+        if(element.assets[0].immutable_data.world == "chikin-kaah" && 	chikinKaah == 0){
+          let saleUrl = saleLink.concat(element.sale_id)
+          console.log(element.sale_id);
+          console.log(element.assets[0].immutable_data.world);
+          console.log(saleUrl);
+          chikinKaah = 1;
+        }
+
+
+      });
+      //console.log(list[0].sale_id);
+      //console.log(list[0].assets[0].immutable_data.world);
+
+      let imgUrl = imgBegin.concat(list[0].assets[0].template.immutable_data.img);
+
+      let saleUrl = saleLink.concat(list[0].sale_id)
+
+      outputLandKeySales.innerHTML += '<center><br><tr><td><img src="' + imgUrl 
+      + '.jpeg" style="display: in-line;margin-left: auto;margin-right: auto;width: 20%;"></td><br><td>  <a href = "' + saleUrl 
+      + '" target = "_blank">Current Lowest Price = ' + list[0].listing_price/100000000 
+      +'</a></td></tr></center>';
+      i++;
+
+    }).catch(function (error) {
+      console.log(error);
+
+    })
+  });
+  outputLandKeySales.innerHTML += "</table>";
 
 
 }
